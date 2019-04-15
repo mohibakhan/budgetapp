e0fd67ad09d63b8ce2c77088def9e03ddce1cc17
Mohib <mohibakhan>
2019-04-15 10:49:46 -0400

    Update Budget UI

--- app.js
+++ app.js
@@ -108,6 +108,10 @@ let UIController = (function() {
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
     expensesContainer: '.expenses__list',
+    budgetLabel: '.budget__value',
+    incomeLabel: '.budget__income--value',
+    expenseLabel: '.budget__expenses--value',
+    percentageLabel: '.budget__expenses--percentage'
   };
 
   return {
@@ -141,7 +145,7 @@ let UIController = (function() {
         document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
-        clearFields: function() {
+    clearFields: function() {
             let fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
 
@@ -152,7 +156,20 @@ let UIController = (function() {
             });
 
             fieldsArr[0].focus();
-        },
+    },
+
+    displayBudget: function(obj) {
+
+        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+        
+        if (obj.percentage > 0 ) {
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+        } else {
+            document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+        }
+    },
 
     getDOMstrings: function() {
       return DOMstrings;
@@ -184,7 +201,7 @@ let controller = (function(budgetCtrl, UICtrl) {
     let budget = budgetCtrl.getBudget();
 
     // display budget on UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
 
   }
 
@@ -214,6 +231,12 @@ let controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log('App Started...');
+      UICtrl.displayBudget({
+        budget:0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+    });
       setupEventListeners();
     }
   };
